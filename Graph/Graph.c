32ebb9ed746a9bc5c24871ca9f5b36e38008784a
YounghoCho <dodghek1@naver.com>
2016-06-05 00:25:34 +0900

    Update Graph.c

--- Graph/Graph.c
+++ Graph/Graph.c
@@ -87,3 +87,15 @@ void PrintGraph(Graph* G){
 		V=V->Next;	//이동!}	
 	printf("\n"); }
 }
+void DFS(Vertex* V){
+	Edge* E= V->AdjacencyList; //모서리 선언
+	printf("%c ",V->Data); //첫번째 정점 출력, Visited
+	V->Visited= Visited; 
+	
+	while(E != NULL) //모서리가 없을때까지 
+	{
+		if(E->Target != NULL && E->Target->Visited==NotVisited) // //모서리의 Target이있고 방문하지 않았으면
+			DFS(E->Target); //꼭짓점 계속 이동 
+		E=E->Next; //다른 모서리로 이동. 
+	}
+}
