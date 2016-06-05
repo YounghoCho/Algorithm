2052e012d189956e9f3ea0632b39c09a17d4c629
YounghoCho <dodghek1@naver.com>
2016-06-06 03:16:07 +0900

    Update BFS(Q).c

--- Graph/BFS(Q).c
+++ Graph/BFS(Q).c
@@ -2,72 +2,42 @@
 #define size 7
 //인접행렬
 int arr[size][size] ={
- 0, 1, 1, 0, 0, 0, 0,
- 0, 0, 0, 1, 1, 0, 0,
- 0, 0, 0, 1, 0, 1, 0,
- 0, 0, 0, 0, 1, 0, 1,
- 0, 0, 0, 0, 0, 0, 1,
- 0, 0, 0, 0, 0, 0, 1,
- 0, 0, 0, 0, 0, 0, 0 };
-
-//초기화
-int i;
-int visited[size] = { 0, };
-int Q[size];
-int front=-1, rear=-1;
+ 0, 1, 0, 0, 0, 0, 1,
+ 0, 0, 1, 0, 1, 0, 0,
+ 0, 0, 0, 1, 0, 0, 0,
+ 0, 0, 0, 0, 0, 0, 0,
+ 0, 0, 1, 1, 0, 0, 0,
+ 0, 0, 0, 1, 0, 0, 0,
+ 0, 0, 0, 0, 0, 1, 0 };
  
-void EnQ(int data)
-{
- rear++;
- Q[rear] = data;
+int i;
+int visited[size]={0,};
+//초간단 Q만들기 
+int Q[size];
+int rear=0, front=0;
+void EnQ(int d){
+	Q[rear++]=d;
 }
-
-int isEmpty()
-{
- if (rear == front)
-  return 1;
- return 0;
+int DeQ(){
+	return Q[front++];	
 }
-
-int DeQ()
-{
- if (isEmpty())
-  return -1;
- else
- {
-  front++;
-  return Q[front];
- }
+//bFS탐색 
+void BFS(int d){
+	EnQ(d);	//시작점 넣으면서 
+	printf("%d ",d); //동시에 찍고
+	visited[d]=1; 
+	while(rear!=front){	//Q가 비워질 때 까지
+		int temp=DeQ(); //한점 빼고 
+		for(i=0; i<size; i++){ //인접한 점들을 넣으면서 
+			if(arr[temp][i]==1 && visited[i]==0){
+				EnQ(i);
+				printf("%d ",i); //동시에 찍는다. 
+				visited[i]=1;
+			}
+		}
+	}
 }
-
-void bfs(int d)
-{
- EnQ(d);
-//방문하지 않은 정점이라면 출력하고, Visited을 1로 표시
- if (visited[d] == 0)
- {
-  printf("%d ", d);
-  visited[d] = 1;
- }
- while(!isEmpty())
- {
-//Queue에 있는 한 정점을 Pop
-  int t = DeQ();
-  for (int i = 0; i < size; i++)
-  {
-//정점이고, 방문하지 않았을 경우 Queue에 Push하고, Visited을 1로 표시
-   if (arr[t][i] == 1 && visited[i] == 0)
-   {
-    EnQ(i);
-    printf("%d ",i);
-	visited[i] = 1;
-   }
-  }
- }
-}
-
-int main()
-{
- bfs(0);
- return 0;
+int main(){
+	BFS(0);
+	return 0;
 }
