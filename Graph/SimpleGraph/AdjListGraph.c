c0b4428d52a5bbd50b679ee1b4819f63881a7b63
YounghoCho <dodghek1@naver.com>
2016-06-29 23:16:06 +0900

    Update AdjListGraph.c

--- Graph/SimpleGraph/AdjListGraph.c
+++ Graph/SimpleGraph/AdjListGraph.c
@@ -1,3 +1,6 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
 #define size 10
 typedef struct node{
   int data;
@@ -21,4 +24,16 @@ void insert_Edge(G* g, int From, int Target){
   gn->next=g->adj[From]; //노드가 포인터배열을(정점배열) 가리키게하고
   g->adj[From]=gn;//정점 인덱스가 새로운 노드를 가리키게 한다. 여기에 노드들이 계속 이어지게하는 비밀이 있다.
 }
+int visited[size]={0,};
+
+void DFS(G* g, int d){
+  GN* gn= g->Adj[d]; //포인터행렬
+  printf("%d ", d)//gn->data가 아님
+  visited[d]=1;
   
+  for(gn; gn; gn=gn->next)
+    if(!visited[gn->data])
+      DFS(g, gn->data);//재귀호출 이기때문에 printf를 또 해주지 않아도 됨.
+}
+
+#endif
