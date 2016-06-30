ebbc6fed7d70ab40fd4c050352c44beb8405ee53
YounghoCho <dodghek1@naver.com>
2016-07-01 00:01:54 +0900

    Update AdjListGraph.c

--- Graph/SimpleGraph/AdjListGraph.c
+++ Graph/SimpleGraph/AdjListGraph.c
@@ -24,6 +24,7 @@ void insert_Edge(G* g, int From, int Target){
   gn->next=g->adj[From]; //노드가 포인터배열을(정점배열) 가리키게하고
   g->adj[From]=gn;//정점 인덱스가 새로운 노드를 가리키게 한다. 여기에 노드들이 계속 이어지게하는 비밀이 있다.
 }
+//방문 행렬 생성
 int visited[size]={0,};
 
 void DFS(G* g, int d){
@@ -35,5 +36,13 @@ void DFS(G* g, int d){
     if(!visited[gn->data])
       DFS(g, gn->data);//재귀호출 이기때문에 printf를 또 해주지 않아도 됨.
 }
-
+//큐생성
+int Q[size];
+int rear=0, front=0;
+void EnQ(int data){
+  Q[rear++]=data;
+}
+int EdQ(){
+  return Q[front++]; 
+}
 #endif
