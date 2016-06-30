aa401dc111744da194c28ae83ae57fe2c281d7fd
YounghoCho <dodghek1@naver.com>
2016-07-01 00:01:06 +0900

    Update AdjArrayGraph.c

--- Graph/SimpleGraph/AdjArrayGraph.c
+++ Graph/SimpleGraph/AdjArrayGraph.c
@@ -25,6 +25,7 @@ void insert_Edge(G* g, int From, int Target){
   g->mat[From][Target]=1;
   g->mat[Target][From]=1;
 }
+//방문 배열 생성
 int visited[size]={0,};
 
 void DFS(G* g, int data){
@@ -35,6 +36,32 @@ void DFS(G* g, int data){
   for(i=0; i<g->n; i++)
     if(!visited[i] && g->mat[data][i]==1)
       DFS(g, i);//재귀호출이므로 BFS처럼 printf를 또 해주지 않아도 된다.
+}
+//큐생성
+int Q[size];
+int rear=0, front=0;
+void EnQ(int data){
+  Q[rear++]=data;
+}
+int EdQ(){
+  return Q[front++]; 
+}
+
+void BFS(G* g, int data){
+	printf("%d ", data);
+	visited[data]=1;
+	EnQ(data);
+	int i;
+	while(!is_empty){
+		int temp=DeQ();
+		for(i=0; i<g->n; i++){
+			if(!visited[i]&&g->mat[temp][i]==1){
+				printf("%d ", i);
+				visited[i]=1;
+				EnQ(i);				
+			}
+		}
+	}
 }
 
 #endif
