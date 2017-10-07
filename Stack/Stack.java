b18013c022b11772d755dc7ae1f69f5e9504594b
Youngho Cho <dodghek1@naver.com>
2017-10-07 17:50:24 +0900

    Update Stack.java

--- Stack/Stack.java
+++ Stack/Stack.java
@@ -1,23 +1,25 @@
+//입력이 push 1, push2, size, empty 같은 형태일때 동작하는 스택
+
 import java.util.*;
 
 class Main
-{
-	static class Stack{
+{	//everyting is in Stack class
+	static class Stack{ 
 		private int size=0;
-		private NODE top=null;//'NULL' is not //initialize NODE //top is pointer
+		private NODE top=null; //'NULL' is not //NODE user datatype can be here //top is a pointer
 
 		private class NODE{
-			int data;
-			private NODE next;//pointer
+			int data; //data can be accessed from outside, so int.
+			private NODE next; //next directs preNODE
 		}
 		public void push(int n){
 			//create NODE
 			NODE node=new NODE();
 			//NODE set
 			node.data=n;
-			node.next=top;
+			node.next=top; //first:next->top(null), Since second:next->top(pre node)
 			//top set
-			top=node; //node has adress value //top&node is NODE type
+			top=node; //top&node is NODE type
 			//size up
 			size++;
 		}
@@ -43,20 +45,19 @@ class Main
 		Stack stack=new Stack();
 
 		Scanner scan=new Scanner(System.in);
-		int n=Integer.parseInt(scan.nextLine());
+		int n=Integer.parseInt(scan.nextLine()); //how many times to repeat, n
 		
 		for(int i=0; i<n; i++){
 			String s=scan.nextLine();
 			//★ This is crazy
 			//can split sentence with space
-			String[] temp = s.split(" ");
-	//			System.out.println(temp[0]);
-	//			System.out.println(temp[1]);
+			String[] temp = s.split(" "); //not ' ' for String
+	//			System.out.println(temp[0]+" "+temp[1]);
 	
-			//s=="input" is false...
+			//s=="input" is flase
 			//★ s.equals("input") is true 
 
-			if(temp[0].equals("push")){
+			if(temp[0].equals("push")){ //not ' ' for String
 				//change cause tmpe[] is String
 				stack.push(Integer.parseInt(temp[1]));
 			}else if(s.equals("pop")){
