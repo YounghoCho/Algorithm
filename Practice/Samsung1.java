8c22dc93584571f0eaef4af409681c64368e68a6
Youngho Cho <dodghek1@naver.com>
2017-10-20 19:03:42 +0900

    설명 보완

--- Practice/Samsung1.java
+++ Practice/Samsung1.java
@@ -8,16 +8,16 @@
 둘째 줄부터 N개의 줄에 종이에 써 있는 수가 주어진다. 
 입력으로 주어지는 수는 1,000을 넘지 않는 자연수이다.
 
-입력       결과
-5 5        19
+입력        결과
+5 5         19
 1 2 3 4 5
 5 4 3 2 1
 2 3 4 5 6
 6 5 4 3 2
 1 2 1 2 1
 
-입력                 결과
-4 10                 7
+입력                  결과
+4 10                  7
 1 2 1 2 1 2 1 2 1 2
 2 1 2 1 2 1 2 1 2 1
 1 2 1 2 1 2 1 2 1 2
@@ -28,40 +28,40 @@ import java.util.*;
 
 class Main 
 {
-	//최대 1000인 점 5개를 구하는 연산을 하기때문에.
-	static int INF=5001;
 	//위,오,아래,왼 순서
 	static int cols[]={1,0,-1,0};
 	static int rows[]={0,1,0,-1};
 	//가로세로 N,M 최대 500
+	static int N,M;
+	//방왔던 길을 되돌아가면 안되기때문에 visited가 빠지면 안됨★ 
 	static int visited[][]=new int[500][500];
 	static int matrix[][]=new int[500][500];
 	//DFS를 4번 돌려야한다.
-	static int result=0,sum=0;
-	static int N,M;
 	static int limit=4;
+	static int result=0,sum=0;
 	
 	public static void main(String[] args) 
 	{
 		Scanner scan=new Scanner(System.in);
-		N= scan.nextInt();
+		N= scan.nextInt(); //int를 또 선언하지 않도록 조심
 		M= scan.nextInt();
 		for(int i=0; i<N; i++){
 			for(int j=0; j<M; j++){
 				matrix[i][j]=scan.nextInt();
 			}
 		}
-		//solve
+		scan.close(); //close꼭쓰고
+		
 		//각정점별로 이동하면서
 		for(int i=0; i<N; i++){
 			for(int j=0; j<M; j++){
-			//깊이탐색 ㄱ,ㄴ 형태찾기(예제1,2)
-			DFS(0,i,j,0);
-			//DFS로 안되는 ㅗ,ㅓ,ㅏ,ㅜ 찾기(예제3)
+			//깊이탐색 ㄱ,ㄴ 형태찾기
+			DFS(0,i,j,0); //4번째 인자는 sum인데, 정점이 움직일 때무다 sum=0으로 자동 초기화된다. 
+			//DFS로 안되는 ㅗ,ㅓ,ㅏ,ㅜ 예외찾기
 			solve(i,j);
 			}
 		}
-		System.out.println("결과: "+result);
+		System.out.println(result);
 	}
   
 	static void DFS(int depth, int row, int col, int sum){
@@ -71,7 +71,7 @@ class Main
 		//print_visited();
 
 		//4번 탐색하고 멈춰야함
-		if(depth==limit-1){
+		if(depth==limit-1){ //3보다는 limit-1이 유지보수 용이
 			result= result<sum? sum: result;
 			//마지막 방문을 취소해줘야, 이전 방문점에서 다시 시작할 수 있음★
 			visited[row][col]=0;
@@ -96,7 +96,9 @@ class Main
 	static void solve(int row, int col){
 		int count=0;
 		int sum=matrix[row][col];
-		int min=INF;
+		//최대 1000인 점 5개를 구하는 연산을 하기때문에.
+		int min=5001;
+		
 		//네방향을 더한다
 		for(int i=0; i<limit; i++){
 			int x=row+rows[i];
@@ -108,6 +110,7 @@ class Main
 			sum+=matrix[x][y];
 			min= min>matrix[x][y]? matrix[x][y]: min;
 		}
+		
 		//모서리인경우는 3개만 더한게 결과다
 		if(count==1)
 			result= result<sum? sum: result;
@@ -128,5 +131,4 @@ class Main
 		}
 		System.out.println();
 	}
-  
 }
